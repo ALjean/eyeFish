5cb63a84438c0cfb17b22385223b2df4dc79e43a
tattoojir <golievalexey@gmail.com>
2015-08-25 15:23:58 +0300

    Has been added new method for convert mmBar to mmHg

--- src/main/java/com/jean/util/Utils.java
+++ src/main/java/com/jean/util/Utils.java
@@ -9,15 +9,18 @@ import java.util.Map;
  */
 public class Utils {
     public static float kelvinToCelsius(float kelvinTemp) {
-        float c = (kelvinTemp - Constants.KELVIN_VALUE) * 1;
+	float c = (kelvinTemp - Constants.KELVIN_VALUE) * 1;
 
-        return round(c);
+	return round(c);
     }
 
-
+    public static int millibarToMmHg(float millibar) {
+	int mmHg = (int) (Constants.MILLIBAR_VALUE * millibar);
+	return mmHg;
+    }
 
     private static float round(float number) {
-        float tmp = number * 100;
-        return (float) (int) ((tmp - (int) tmp) >= 0.5f ? tmp + 1 : tmp) / 100;
+	float tmp = number * 100;
+	return (float) (int) ((tmp - (int) tmp) >= 0.5f ? tmp + 1 : tmp) / 100;
     }
 }
