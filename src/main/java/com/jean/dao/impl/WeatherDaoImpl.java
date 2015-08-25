e34c3e4cd140933415b26c9a27cbe56b24c9b85b
tattoojir <golievalexey@gmail.com>
2015-08-25 19:05:42 +0300

    Has been added new method - get weather for data limit

--- src/main/java/com/jean/dao/impl/WeatherDaoImpl.java
+++ src/main/java/com/jean/dao/impl/WeatherDaoImpl.java
@@ -20,57 +20,91 @@ public class WeatherDaoImpl extends BaseDaoImpl implements WeatherDao {
 
     @Override
     public void save(Weather weather) throws CustomDfmException {
-        String sql = "INSERT INTO weather (city, date, temp_day, temp_night, temp_even, temp_morn, pressure, wind_speed, wind_deg, humidity, clouds) " +
-                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	String sql = "INSERT INTO weather (city, date, temp_day, temp_night, temp_even, temp_morn, pressure, wind_speed, wind_deg, humidity, clouds) "
+		+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        try (PreparedStatement preparedStatement = getConnection().prepareStatement(sql)) {
-            preparedStatement.setString(1, weather.getCity());
-            preparedStatement.setDate(2, new Date(weather.getDate().getTime()));
-            preparedStatement.setFloat(3, weather.getTempDay());
-            preparedStatement.setFloat(4, weather.getTempNight());
-            preparedStatement.setFloat(5, weather.getTempEven());
-            preparedStatement.setFloat(6, weather.getTempMorn());
-            preparedStatement.setFloat(7, weather.getPressure());
-            preparedStatement.setFloat(8, weather.getWindSpeed());
-            preparedStatement.setFloat(9, weather.getWindDeg());
-            preparedStatement.setInt(10, weather.getHumidity());
-            preparedStatement.setInt(11, weather.getClouds());
-            int result = preparedStatement.executeUpdate(); //todo
-        } catch (SQLException e) {
-            throw new CustomDfmException(e, "some problem with save weather");
-        }
+	try (PreparedStatement preparedStatement = getConnection().prepareStatement(sql)) {
+	    preparedStatement.setString(1, weather.getCity());
+	    preparedStatement.setDate(2, new Date(weather.getDate().getTime()));
+	    preparedStatement.setFloat(3, weather.getTempDay());
+	    preparedStatement.setFloat(4, weather.getTempNight());
+	    preparedStatement.setFloat(5, weather.getTempEven());
+	    preparedStatement.setFloat(6, weather.getTempMorn());
+	    preparedStatement.setFloat(7, weather.getPressure());
+	    preparedStatement.setFloat(8, weather.getWindSpeed());
+	    preparedStatement.setFloat(9, weather.getWindDeg());
+	    preparedStatement.setInt(10, weather.getHumidity());
+	    preparedStatement.setInt(11, weather.getClouds());
+	    int result = preparedStatement.executeUpdate(); // todo
+	} catch (SQLException e) {
+	    throw new CustomDfmException(e, "some problem with save weather");
+	}
 
     }
 
-    public List<Weather> getAllWeather() throws CustomDfmException{
-        String sql = "SELECT * FROM weather";
-        List<Weather> weathers = new ArrayList<>();
+    public List<Weather> getAllWeather() throws CustomDfmException {
+	String sql = "SELECT city, date, temp_day, temp_night, temp_even, temp_morn, pressure, wind_speed, wind_deg, humidity, clouds FROM weather";
+	List<Weather> weathers = new ArrayList<>();
 
-        try(PreparedStatement preparedStatement = getConnection().prepareStatement(sql)) {
-            ResultSet rs = preparedStatement.executeQuery();
+	try (PreparedStatement preparedStatement = getConnection().prepareStatement(sql)) {
+	    ResultSet rs = preparedStatement.executeQuery();
 
-            while (rs.next()){
-                Weather weather = new Weather();
-                weather.setId(rs.getInt("id"));
-                weather.setCity(rs.getString("city"));
-                weather.setDate(rs.getDate("date"));
-                weather.setTempDay(rs.getFloat("temp_day"));
-                weather.setTempEven(rs.getFloat("temp_even"));
-                weather.setTempMorn(rs.getFloat("temp_morn"));
-                weather.setTempNight(rs.getFloat("temp_night"));
-                weather.setPressure(rs.getFloat("pressure"));
-                weather.setWindSpeed(rs.getFloat("wind_speed"));
-                weather.setWindDeg(rs.getFloat("wind_deg"));
-                weather.setHumidity(rs.getInt("humidity"));
-                weather.setClouds(rs.getInt("clouds"));
+	    while (rs.next()) {
+		Weather weather = new Weather();
+		weather.setId(rs.getInt("id"));
+		weather.setCity(rs.getString("city"));
+		weather.setDate(rs.getDate("date"));
+		weather.setTempDay(rs.getFloat("temp_day"));
+		weather.setTempEven(rs.getFloat("temp_even"));
+		weather.setTempMorn(rs.getFloat("temp_morn"));
+		weather.setTempNight(rs.getFloat("temp_night"));
+		weather.setPressure(rs.getFloat("pressure"));
+		weather.setWindSpeed(rs.getFloat("wind_speed"));
+		weather.setWindDeg(rs.getFloat("wind_deg"));
+		weather.setHumidity(rs.getInt("humidity"));
+		weather.setClouds(rs.getInt("clouds"));
 
-                weathers.add(weather);
-            }
+		weathers.add(weather);
+	    }
 
-        }catch (SQLException e){
-            throw new CustomDfmException(e, "some problem when get List Weather");
-        }
+	} catch (SQLException e) {
+	    throw new CustomDfmException(e, "some problem when get List Weather");
+	}
 
-        return weathers;
+	return weathers;
+    }
+
+    @Override
+    public List<Weather> getLimitWeatherByDate(java.util.Date date) throws CustomDfmException {
+	String sql = "SELECT city, date, temp_day, temp_night, temp_even, temp_morn, pressure, wind_speed, wind_deg, humidity, clouds FROM weather where date <= ? limit 4";
+
+	List<Weather> weathers = new ArrayList<>();
+
+	try (PreparedStatement preparedStatement = getConnection().prepareStatement(sql)) {
+	    ResultSet rs = preparedStatement.executeQuery();
+
+	    while (rs.next()) {
+		Weather weather = new Weather();
+		weather.setId(rs.getInt("id"));
+		weather.setCity(rs.getString("city"));
+		weather.setDate(rs.getDate("date"));
+		weather.setTempDay(rs.getFloat("temp_day"));
+		weather.setTempEven(rs.getFloat("temp_even"));
+		weather.setTempMorn(rs.getFloat("temp_morn"));
+		weather.setTempNight(rs.getFloat("temp_night"));
+		weather.setPressure(rs.getFloat("pressure"));
+		weather.setWindSpeed(rs.getFloat("wind_speed"));
+		weather.setWindDeg(rs.getFloat("wind_deg"));
+		weather.setHumidity(rs.getInt("humidity"));
+		weather.setClouds(rs.getInt("clouds"));
+
+		weathers.add(weather);
+	    }
+
+	} catch (SQLException e) {
+	    throw new CustomDfmException(e, "some problem when get List Weather");
+	}
+
+	return weathers;
     }
 }
