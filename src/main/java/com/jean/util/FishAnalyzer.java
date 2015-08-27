0de8350f05087b77163cf35cec4980a02c5e2b19
tattoojir <golievalexey@gmail.com>
2015-08-27 16:43:25 +0300

    Has been created FishAnalyzer 1.0 (beta version)

--- src/main/java/com/jean/util/FishAnalyzer.java
+++ src/main/java/com/jean/util/FishAnalyzer.java
@@ -1,29 +1,12 @@
 package com.jean.util;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.jean.CustomDfmException;
-import com.jean.dao.WeatherDao;
-import com.jean.entity.Weather;
+import com.jean.entity.Fish;
 
-public class FishAnalyzer {
-    
-    @Autowired
-    WeatherDao weatherDao;
+public interface FishAnalyzer {
 
-    public List getHungryRating(Date date) throws CustomDfmException{
-	
-	Map<String, Integer> ratingList = new HashMap<String,Integer>();
-	
-	List<Weather> listWeather = weatherDao.getAllWeather();
-	
-	return null;
-	
-    }
+    public List<String> getHungryRating(Fish fish) throws CustomDfmException;
 }
