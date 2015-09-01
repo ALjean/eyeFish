33e6b1dd3823e1b44020c4dedfb8f9b15764e398
tattoojir <golievalexey@gmail.com>
2015-09-01 16:56:48 +0300

    new analyzer

--- src/main/java/com/jean/util/FishAnalyzerImpl.java
+++ src/main/java/com/jean/util/FishAnalyzerImpl.java
@@ -14,36 +14,51 @@ import com.jean.dao.WeatherDao;
 import com.jean.dao.impl.WeatherDaoImpl;
 import com.jean.entity.Fish;
 import com.jean.entity.Weather;
-import com.jean.entity.WeatherState;
+import com.jean.entity.RangeWeatherParameters;
 
 @Component
 public class FishAnalyzerImpl implements FishAnalyzer {
-    
-   @Autowired
-    private WeatherDao weatherDao;
 
-    public List<String> getHungryRating(Fish fish) throws CustomDfmException{
-	
+    @Autowired
+    private WeatherDao weatherDao;
+    
+    private Map<String, Integer> raiting;
+
+    public List<String> getHungryRating(Fish fish) throws CustomDfmException {
+
 	List<Weather> listWeather = new ArrayList<Weather>();
-		
+
 	listWeather = weatherDao.getLimitWeatherByDate();
-	
-	Map<String, WeatherState> hungry = fish.getHungry();
-	
+
+	Map<String, List<RangeWeatherParameters>> hungry = fish.getHungry();
+
 	List<String> listState = new ArrayList<String>();
-	
-	for(Weather weather : listWeather){
-	    
-	    for(Map.Entry<String, WeatherState> entry : hungry.entrySet()){
+
+	for (Weather weather : listWeather) {
+
+	    for (Map.Entry<String, List<RangeWeatherParameters>> entry : hungry.entrySet()) {
 		
-		if(entry.getValue().getMin() < weather.getTempDay() || entry.getValue().getMax() < weather.getTempDay()){
-		    listState.add(entry.getKey());
+		for(RangeWeatherParameters param : entry.getValue()){
+		    if(param.getMin() < weather.getTempDay() || param.getMax() < weather.getTempDay()){
+			//raiting.put(entry.getKey(), new Integer(value));
+		    }
 		}
-		
 	    }
+
 	}
-	
+
 	return listState;
-	
+
     }
+
+    public FishAnalyzerImpl() { 
+	
+	this.raiting = new HashMap<String, Integer>();
+	for (Constants.StateMarkHungry stateMark : Constants.StateMarkHungry.values()) {
+	    raiting.put(stateMark.toString(), null);
+	}
+    }
+    
+    
+
 }
