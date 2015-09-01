0bd4eb006883cd1bb76f4ffbe1c860d3e96d6ee6
tattoojir <golievalexey@gmail.com>
2015-09-01 16:58:25 +0300

    new fish

--- src/main/java/com/jean/entity/Fish.java
+++ src/main/java/com/jean/entity/Fish.java
@@ -1,76 +1,96 @@
 package com.jean.entity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import com.jean.util.Constants;
 
-/**
- * Created by Stas Patek and Aleksey Gloliev and Some Guy With Big Head on 30.05.15.
- */
 public class Fish {
 
-	private long id;
+    private int id;
 
-	private String name;
+    private String name;
+
+    private String description;
+
+    private Spawning spawning;
+
+    private Map<String, List<RangeWeatherParameters>> hungry;
+
+    private Map<String, List<RangeWeatherParameters>> location;
+
+    public int getId() {
+	return id;
+    }
+
+    public void setId(int id) {
+	this.id = id;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public String getDescription() {
+	return description;
+    }
+
+    public void setDescription(String description) {
+	this.description = description;
+    }
+
+    public Spawning getSpawning() {
+	return spawning;
+    }
+
+    public void setSpawning(Spawning spawning) {
+	this.spawning = spawning;
+    }
+
+    public Map<String, List<RangeWeatherParameters>> getHungry() {
+	return hungry;
+    }
+
+    public void setHungry(Map<String, List<RangeWeatherParameters>> hungry) {
+	this.hungry = hungry;
+    }
+
+    public Map<String, List<RangeWeatherParameters>> getLocation() {
+	return location;
+    }
+
+    public void setLocation(Map<String, List<RangeWeatherParameters>> location) {
+	this.location = location;
+    }
+
+   
+    @Override
+    public String toString() {
+	return "Fish [id=" + id + ", name=" + name + ", description=" + description + ", spawning=" + spawning + ", hungry=" + hungry + ", location="
+		+ location + "]";
+    }
+
+    public Fish() {
+	hungry = new HashMap<String, List<RangeWeatherParameters>>();
+	location = new HashMap<String, List<RangeWeatherParameters>>();
 	
-	private Map<String, WeatherState> hungry;
-	
-	private Map<String, WeatherState> location;
-
-	private Spawning spawning;
-	
-	private String description;
-
-	public long getId() {
-		return id;
+	for (Constants.StateMarkHungry stateMark : Constants.StateMarkHungry.values()) {
+	    hungry.put(stateMark.toString(), new ArrayList<RangeWeatherParameters>());
 	}
-
-	public void setId(long id) {
-		this.id = id;
+	for(Constants.StateMarkLocation stateMark : Constants.StateMarkLocation.values()){
+	    location.put(stateMark.toString(), new ArrayList<RangeWeatherParameters>());
 	}
-
-	public Spawning getSpawning() {
-		return spawning;
-	}
-
-	public void setSpawning(Spawning spawning) {
-		this.spawning = spawning;
-	}
-
-	public Map<String, WeatherState> getHungry() {
-		return hungry;
-	}
-
-	public void setHungry(Map<String, WeatherState> hungry) {
-		this.hungry = hungry;
-	}
-
-	public Map<String, WeatherState> getLocation() {
-		return location;
-	}
-
-	public void setLocation(Map<String, WeatherState> location) {
-		this.location = location;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public String toString() {
-		return "Fish [id=" + id + ", hungry=" + hungry + ", location="
-				+ location + ", description=" + description + "]";
-	}
+    }
+    
+    public static void main(String[] args){
+	Fish fish = new Fish();
+	System.out.println(fish.getHungry());
+    }
 
 }
